ffedda07e44a93c1a4ee48e187068f88518efa5d
Bruna Gomes da Silva <brugomes@users.noreply.github.com>
2020-01-04 17:53:22 +1300

    Dist folder organization and gulpfile config

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,51 @@
-const { series, watch } = require('gulp');
+const { series, parallel, watch } = require('gulp');
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync').create(),
     merge = require('merge-stream'),
     rename = require("gulp-rename"),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    fs = require('fs');
+
+function cleanFolder(){
+  try {
+      fs.statSync('dist');
+      return gulp.src('dist')
+        .pipe(clean({force: true}));
+  }
+  catch (err) {
+    if (err.code === 'ENOENT') {
+      return gulp.src('.');
+    }
+  }
+}
+
+function copy(){
+  return gulp.src(['index.html', 'src/**', '!src/sass/**', '!src/js/**'])
+    .pipe(gulp.dest('dist/'))
+}
 
 function styles() {
-  return gulp.src('./src/sass/index.scss')
+  return gulp.src('src/sass/index.scss')
     .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 }
 
 function js() {
-    var fontawesome =  gulp.src('./node_modules/@fortawesome/fontawesome-free/js/all.min.js')
+    var fontawesome =  gulp.src('node_modules/@fortawesome/fontawesome-free/js/all.min.js')
       .pipe(rename("fontawesome.min.js"))
-      .pipe(gulp.dest('./dist/js'));
+      .pipe(gulp.dest('dist/js'));
 
-    var jquery =  gulp.src('./node_modules/jquery/dist/jquery.min.js')
-      .pipe(gulp.dest('./dist/js'));
+    var jquery =  gulp.src('node_modules/jquery/dist/jquery.min.js')
+      .pipe(gulp.dest('dist/js'));
 
-    var script =  gulp.src('./src/js/*.js')
+    var script =  gulp.src('src/js/*.js')
       .pipe(rename("script.min.js"))
       .pipe(uglify())
-      .pipe(gulp.dest('./dist/js'));
+      .pipe(gulp.dest('dist/js'));
 
     return merge(fontawesome, jquery, script);
 }
@@ -33,15 +53,13 @@ function js() {
 function startBrowser(){
   return browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: 'dist'
     }
   });
 }
 
-watch(['./src/sass/**/*.scss', ], function() {
+watch(['src/sass/**/*.scss', ], function() {
   return styles();
 });
 
-exports.styles = styles;
-exports.startBrowser = startBrowser;
-exports.default = series(styles, js, startBrowser);
+exports.default = series(cleanFolder, parallel(copy, styles, js, startBrowser));
